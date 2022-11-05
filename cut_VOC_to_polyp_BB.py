9272a8543aa419622c6cfbe4d735291381fe1189
Joshua Friedrich <joshua.friedrich@stud-mail.uni-wuerzburg.de>
2022-11-05 21:12:17 +0100

    analyse funktionen über Resolution und Polyp size hinzugefügt

--- cut_VOC_to_polyp_BB.py
+++ cut_VOC_to_polyp_BB.py
@@ -1,9 +1,11 @@
+import glob
 import os
 import shutil
 from PIL import Image, ExifTags
 from pathlib import Path
 import xml.etree.ElementTree as ET
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 ''' Cutting VOC-Detection datasets to the polyps, so no Background is left
 reads voc annotation files and cuts the images according to the bounding boxes
@@ -119,7 +121,79 @@ def cut_voc_to_bb( data_path, dest_path ):
 
 
 
+# find all unique directory given in the "path"-parameter of all xmls
+# useful to maybe use the "train"-data from KUMC
+def get_unique_paths( data_path):
+    xml_files = [f for f in glob.glob(data_path + "/**/*.xml", recursive=True)]
+
+    print("glob done")
+
+    paths = sorted({os.path.dirname(root.find('path').text) for xml in xml_files if (root := ET.parse(xml).getroot())})
+
+    print(paths)
+
+
+
+# analyze the composition of the various resolutions and polyp sizes based on the xml data found
+def analyze_voc_sizes( data_path ):
+
+    xml_files = [f for f in glob.glob(data_path + "/**/*.xml", recursive=True)]
+    # jpg_files = [f for f in glob.glob(data_path + "/**/*.jpg", recursive=True)]
+
+    print("glob done")
+
+    # count unique image sizes from the xml given width and height
+    img_sizes_xml = [(int(root.find('size/width').text), int(root.find('size/height').text)) for xml in xml_files if (root := ET.parse(xml).getroot())]
+    # img_sizes_jpg = {Image.open(img).size for img in jpg_files if Image.open(img).size}
+    img_sizes = np.array(img_sizes_xml)
+    sizes_values, sizes_counts = np.unique(img_sizes, axis=0, return_counts=True)
+    print("Image resolutions : count")
+    for size in zip(sizes_values, sizes_counts):
+        print(size[0], ": ", size[1])
+
+
+
+    # create hist of polyp sizes based on polyp BB coords given in xmls
+    # to many unique sizes to count each exact size, so create histogram
+    polyp_sizes_xml = []
+    for xml in xml_files:
+        root = ET.parse(xml).getroot()
+        # frames without polyp have no bndbox, so need to check
+        if ET.parse(xml).getroot().find('object/bndbox/xmin') is not None:
+            polyp_sizes_xml.append((int(root.find('object/bndbox/xmax').text) - int(
+                root.find('object/bndbox/xmin').text), int(root.find('object/bndbox/ymax').text) - int(
+                root.find('object/bndbox/ymin').text)))
+
+    polyp_sizes = np.array(polyp_sizes_xml)
+
+    # 1D hist
+    _ = plt.hist(polyp_sizes, bins='auto')  # arguments are passed to np.histogram
+    plt.title("Polyp sizes")
+    plt.show()
+
+    # 2D hist
+    plt.title("Polyp sizes")
+    H, xedges, yedges = np.histogram2d(polyp_sizes[:, 0], polyp_sizes[:, 1], bins=50)
+    # Histogram does not follow Cartesian convention (see Notes),
+    # therefore transpose H for visualization purposes.
+    H = H.T
+    X, Y = np.meshgrid(xedges, yedges)
+
+    plt.pcolormesh(X, Y, H)
+    plt.show()
+
+
+
+    print("done")
+
+
+
+
+
 if __name__ == '__main__':
-    cut_voc_to_bb(data_path="D:\\Master_Daten\\KUMC_PolypsSet\\PolypsSet\\val2019",
-                  dest_path="D:\\Master_Daten\\KUMC_PolypsSet\\PolypsSet_cut\\val2019")
+    # cut_voc_to_bb(data_path="D:\\Master_Daten\\KUMC_PolypsSet\\PolypsSet\\val2019",
+    #               dest_path="D:\\Master_Daten\\KUMC_PolypsSet\\PolypsSet_cut\\val2019")
+
+    analyze_voc_sizes("D:\\Master_Daten\\KUMC_PolypsSet\\PolypsSet\\val2019")
+    # get_unique_paths("KUMC_PolypsSet/PolypsSet/train2019")
     exit(0)
