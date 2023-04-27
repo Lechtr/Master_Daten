307a4cde2596956e7c94fe21b310b29274776c8c
Joshua Friedrich <joshua.friedrich@stud-mail.uni-wuerzburg.de>
2023-04-27 21:33:40 +0200

    add sort numerically

--- VOC_utils/rename_VOC_files_to_numerically.py
+++ VOC_utils/rename_VOC_files_to_numerically.py
@@ -5,10 +5,12 @@ from pathlib import Path
 
 
 
-def rename_VOC_files_to_numerically( data_path ):
+def rename_VOC_files_to_numerically( data_path, sort_numerical=False):
     """
     rename all files in numerical order
     so all frames of a video start with frame/file 1.jpg
+    :param sort_numerical: if false sorts filenames by default alphabetically, so 1,11,12,13...,2,21,22,....
+    if true assumes that the filenames are ints and sorts numerically, so 1,2,3,4,....
     :param data_path:
     :return:
     """
@@ -21,16 +23,21 @@ def rename_VOC_files_to_numerically( data_path ):
             i = 1
 
             # iter through files in vid
-            for img in sorted(vid_dir.glob("*.jpg")):
+            if sort_numerical:
+                img_files = sorted(vid_dir.glob("*.jpg"), key=lambda file: int(file.stem))
+            else:
+                img_files = sorted(vid_dir.glob("*.jpg"))
+
+            for img in img_files:
                 anno_file = Path(str(img).replace("Image", "Annotation")).with_suffix(".xml")
 
-                print(img)
+                # print(img)
                 img = img.rename(img.with_stem(str(i)))
-                print("renamed to: ", img)
+                print(img, " renamed to: ", img.with_stem(str(i)))
 
-                print(anno_file)
+                # print(anno_file)
                 anno_file = anno_file.rename(anno_file.with_stem(str(i)))
-                print("renamed to: ", anno_file)
+                # print("renamed to: ", anno_file)
 
                 i += 1
 
@@ -41,6 +48,6 @@ def rename_VOC_files_to_numerically( data_path ):
 if __name__ == '__main__':
     os.chdir("D:\Master_Daten")
 
-    # remove_files_without_image_or_annotation("KUMC_PolypsSet/")
-    rename_VOC_files_to_numerically("processed_data/SUN/Image/")
+    # rename_VOC_files_to_numerically("processed_data/SUN/Image/", sort_numerical=False)
+    rename_VOC_files_to_numerically("processed_data/KUMC/Image", sort_numerical=True)
     exit(0)
